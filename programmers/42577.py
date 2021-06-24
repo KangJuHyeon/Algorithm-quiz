c9cc6dacf7fb8f9cf7e8f62e80c159515d31f61d
KANGJUHYEON <alinos@naver.com>
2021-06-24 23:10:04 +0900

    Hash

--- programmers/42577.py
+++ programmers/42577.py
@@ -1,15 +1,11 @@
 def solution(phone_book):
-    answer = True
     phone_book.sort()
-
-    for phone in range(len(phone_book)-1):
-        if phone_book[phone] in phone_book[phone+1]:
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        if p2.startswith(p1):
             return False
+    return True
 
-    return answer
-
-# 만약 정렬을 한다면 자연스럽게 짧은 길이의 문자열이 앞으로 온다.
-# 비교도 앞-뒤 만 하면 되기 때문에 쉽게 풀 수 있다.
-
-# 내가 이해가 안됐던 부분은 range(len(phone_book)-1) 이 부분이었는데 
-# phone_book 배열에 -1 을 하는 이유는 인덱스 수를 세려면 -1 을 하는 것이 맞다. 이 부분이 이해가 안됐다는게 부끄럽다.
+# 내장함수 zip을 사용한 풀이
+# zip은 함수 안의 각 리스트, 튜플, 문자열에 대하여 각 요소를 짝지어 주는 함수이다.
+# 반환 값이 list는 아니기 때문에 for문의 변수에 대입하는 것이 아니라면 list처리를 해줘야 한다.
+# startswith는 꽤 직관적인 함수로 p2가 p1으로 시작되면 True 아니면 False를 반환한다.
