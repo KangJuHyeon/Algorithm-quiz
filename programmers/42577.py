526bcac7800350f087a817cdba1fd06a4eeb41fd
KANGJUHYEON <alinos@naver.com>
2021-06-24 23:12:52 +0900

    Hash Table

--- programmers/42577.py
+++ programmers/42577.py
@@ -1,11 +1,19 @@
 def solution(phone_book):
-    phone_book.sort()
-    for p1, p2 in zip(phone_book, phone_book[1:]):
-        if p2.startswith(p1):
-            return False
-    return True
-
-# 내장함수 zip을 사용한 풀이
-# zip은 함수 안의 각 리스트, 튜플, 문자열에 대하여 각 요소를 짝지어 주는 함수이다.
-# 반환 값이 list는 아니기 때문에 for문의 변수에 대입하는 것이 아니라면 list처리를 해줘야 한다.
-# startswith는 꽤 직관적인 함수로 p2가 p1으로 시작되면 True 아니면 False를 반환한다.
+    answer = True
+    hash_map = {} # 딕셔너리
+    
+    # 등장한 숫자들을 count 딕셔너리로 만든다.
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+        
+    # 다시 숫자들을 꺼낸뒤
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number: #숫자 하나씩 뜯어보기
+            temp += number
+            # 딕셔너리 키로 같은게 있지만 전체 숫자는 다른 경우
+            if temp in hash_map and temp != phone_number:
+                answer = False
+                
+    print(hash_map)
+    return answer
