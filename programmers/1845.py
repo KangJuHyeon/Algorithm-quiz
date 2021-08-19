326ca5e66b824a86818828897318ac00f68fe717
KANGJUHYEON <alinos@naver.com>
2021-08-19 16:04:08 +0900

    폰켓몬

--- programmers/1845.py
+++ programmers/1845.py
@@ -40,9 +40,9 @@ def solution(nums):
     answer = len(nums) / 2
     for num in nums:
         if num not in arr:
-            arr.append(num)
+            arr.append(num) # [3], [3, 1] || [3], [3, 2], [3, 2, 4] || [3], [3, 2]
         if answer > len(arr):
-            result = len(arr)
+            result = len(arr) # 1,2.0 || 1,1,1,2,2,3.0 || 1,1,1,2,2,2,2
         else:
             return answer
     return result
