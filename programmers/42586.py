1679f9eb7b040572a6db3236dac3405ac9bfb038
KANGJUHYEON <alinos@naver.com>
2021-10-01 00:00:26 +0900

    기능개발

--- programmers/42586.py
+++ programmers/42586.py
@@ -1,22 +1,27 @@
 # queue FIFO 을 활용한 풀이
 def solution(progresses, speeds):
-    answer = []
-    days = 0
-    count = 0
+    answer = [] # 기능 배포한 갯수를 담을 상자
+    days = 0  # 걸린 날짜
+    count = 0 # 기능을 배포한 갯수
 
-    while len(progresses) != 0:
-        if progresses[0] + days * speeds[0] >= 100:
+    while len(progresses) != 0: # 진행상황이 0이 아니라면
+        if (progresses[0] + days * speeds[0]) >= 100: # 100이 넘어가면 완료된 것이니까 더 크거나 같다면 조건문을 주었다.
             progresses.pop(0)
+            # print(progresses)
             speeds.pop(0)
+            # print(speeds)
             count += 1
-        elif count > 0:
-            answer.append(count)
-            count = 0
-        days += 1
-        
-        answer.append(count)
-    return answer
+        else:
+            if count > 0:
+                answer.append(count)
+                print(answer)
+                count = 0
+            days += 1
 
+    answer.append(count)
+
+    return answer
+print(solution([93, 30, 55], [1, 30, 5]))
 # progresses(작업 진도), speeds(작업 속도), return 값(배포될 때 기능이 완료된 것들)
 # progresses(작업 진도)가 0이 아니라면 while문에 들어가고
 # 만약 progresses(작업 진도) 리스트 0번 째 인덱스 + days + speeds(작업 속도) 리스트 0번 쨰 인덱스가 100 이하 또는 같다면
@@ -28,27 +33,32 @@ def solution(progresses, speeds):
 import math
 
 def solution(progresses, speeds):
-    answer = []
-    stack = []
-    days = 0
+    answer = [] # 몇개의 기능을 담을 상자
+    stack = []  # 기능에 걸린 날짜
+    days = 0    # 일수 카운트
     
     for i in range(len(progresses)):
         days = math.ceil((100 - progresses[i]) / speeds[i])
-        
+
         if i == 0:
             stack.append(days)
+            # print(stack)
         else:
             if stack[0] >= days:
                 stack.append(days)
-            else:
+                # print(stack)
+            else: # 배포일이 stack에 있는 배포일보다 클 경우 stack pop
                 answer.append(len(stack))
+                # print(answer)
                 stack.clear()
+                # print(stack)
                 stack.append(days)
+                # print(stack)
                 
     answer.append(len(stack))
         
     return answer
-
+print(solution([93, 30, 55], [1, 30, 5]))
 # 앞에 작업보다 뒷작업이 먼저 끝나면 앞에 작업이 끝날때 까지 기다려야 하므로 각 progresses 마다 배포일을 구한다음,  
 # progress 순서대로 배포일을 스택에 담았을때 
 # 다음 배포일이 스택에 있는 배포일보다 작다면 스택에 쌓고 다음 배포일이 스택에 있는 배포일보다 크다면, 
