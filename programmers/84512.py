01e19ba00c398a4079f948b4e5cb23dbe0be851a
KANGJUHYEON <alinos@naver.com>
2022-01-19 22:43:05 +0900

    모음사전

---
+++ programmers/84512.py
@@ -0,0 +1,19 @@
+# 문제풀이(1)
+import itertools
+
+def solution(word):
+    list1 = ['A','E','I','O','U']
+    list2 = list(map(list, itertools.product('AEIOU',repeat=2)))
+    list3 = list(map(list, itertools.product('AEIOU',repeat=3)))
+    list4 = list(map(list, itertools.product('AEIOU',repeat=4)))
+    list5 = list(map(list, itertools.product('AEIOU',repeat=5)))
+    data_list = list1 + list2 + list3 + list4 + list5
+    for i in range(len(data_list)):
+        data_list[i] = ''.join(data_list[i])
+    data_list.sort()
+
+    return data_list.index(word)+1
+print(solution("AAAAE"))  # 6
+print(solution("AAAE")) # 10
+print(solution("I"))    # 1563
+print(solution("EIO"))  # 1189
