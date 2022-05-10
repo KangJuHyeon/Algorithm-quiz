75ed847e060e777028339dd1b803aa01c326f144
Kangjuhyeon <alinos@naver.com>
2022-05-10 17:52:54 +0900

    9. Palindrome Number

---
+++ leetcode/easy/palindromeNumber.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
+
+print(Solution().isPalindrome(121))
+print(Solution().isPalindrome(-121))
+print(Solution().isPalindrome(10))
