416941bf17c8b6f4d2c81b244ef89d9fd891329f
Kangjuhyeon <alinos@naver.com>
2022-06-06 22:09:02 +0900

    덱

---
+++ baekjoon/Algorithm_basic/10866.py
@@ -0,0 +1,24 @@
+# push_front X: 정수 X를 덱의 앞에 넣는다.
+# push_back X: 정수 X를 덱의 뒤에 넣는다.
+# pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# size: 덱에 들어있는 정수의 개수를 출력한다.
+# empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
+# front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+from collections import deque
+
+queue = deque()
+
+for i in range(10):
+    queue.append(i)
+print(queue)
+
+queue.appendleft(10)
+print(queue)
+
+queue.insert(5, 11)
+print(queue)
+
+queue.extend([0,0,0])
