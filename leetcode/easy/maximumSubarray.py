0510f10296e466ef0dd246cf3135834ab1200069
Kangjuhyeon <alinos@naver.com>
2022-06-09 21:24:17 +0900

    modify file

---
+++ leetcode/easy/maximumSubarray.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        answer = []
+        
+        return answer
+
+print(Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4])) # 6
